dae153370f7b06ac61abd75a898c2e742f77c614
akashtinu <yakashtinu@gmail.com>
2026-06-14 17:26:05 +0530

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://emp-backend-to7x.onrender.com/api',
+  baseURL: 'https://emp-backend-1o7x.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
@@ -9,4 +10,3 @@ export const getUsers   = ()         => api.get('/users');
 export const createUser = (data)     => api.post('/users', data);
 export const updateUser = (id, data) => api.put(`/users/${id}`, data);
 export const deleteUser = (id)       => api.delete(`/users/${id}`);
-
